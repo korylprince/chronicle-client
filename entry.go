cc98b4175fb7914aca8a73e49dd6d3d07cb09af5
Kory Prince <korylprince@gmail.com>
2020-02-27 13:39:06 -0600

    fix timing issues so local IP can be used in request

--- entry.go
+++ entry.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+)
+
 //Entry represents information about a computer
 type Entry struct {
 	UID              uint32 `json:"uid"`
@@ -9,4 +14,22 @@ type Entry struct {
 	ClientIdentifier string `json:"client_identifier"`
 	Hostname         string `json:"hostname"`
 	IP               string `json:"ip"`
+}
+
+// Close implements io.Closer
+func (e *Entry) Close() error {
+	return nil
+}
+
+// Close implements io.Reader
+func (e *Entry) Read(p []byte) (int, error) {
+	buf := new(bytes.Buffer)
+
+	enc := json.NewEncoder(buf)
+	err := enc.Encode(e)
+	if err != nil {
+		return 0, err
+	}
+
+	return buf.Read(p)
 }
--- send.go
+++ send.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -18,34 +16,26 @@ func Submit() error {
 	entry := Collect()
 
 	//a custom dialer is required to get local IP
-	var req *http.Request
-	dial := func(network, addr string) (net.Conn, error) {
-		conn, err := net.Dial(network, addr)
+	dial := func(context context.Context, network, addr string) (net.Conn, error) {
+		conn, err := (&net.Dialer{}).DialContext(context, network, addr)
 		if err != nil {
 			return conn, err
 		}
+
 		entry.IP = conn.LocalAddr().(*net.TCPAddr).IP.String()
-		var buf bytes.Buffer
-		e := json.NewEncoder(&buf)
-		err = e.Encode(entry)
-		if err != nil {
-			return conn, err
-		}
-		req.Body = ioutil.NopCloser(&buf)
-		req.ContentLength = int64(buf.Len())
+
 		return conn, err
 	}
 
-	t := http.Transport{Dial: dial}
+	t := http.Transport{DialContext: dial}
 	client := http.Client{Transport: &t, Timeout: time.Second * 5}
 
-	var err error
-	//body will be set by custom dialer
-	req, err = http.NewRequest("POST", config.proto+"://"+config.Chronicler+apiPrefix+"/submit", nil)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s://%s%s/submit", config.proto, config.Chronicler, apiPrefix), nil)
 	if err != nil {
 		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
+	req.Body = entry
 
 	resp, err := client.Do(req)
 	if err != nil {
